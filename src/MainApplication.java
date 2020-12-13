0b3460bb39097c7c924fe2217d5517dd2d822166
Prosper Ekwerike <pekwerike@gmail.com>
2020-12-13 20:02:27 +0100

    transferVideo function: completed part to write video into client outputstream

--- src/MainApplication.java
+++ src/MainApplication.java
@@ -14,6 +14,15 @@ public class MainApplication {
         BufferedOutputStream clientBOS = new BufferedOutputStream(clientOS);
         DataOutputStream clientDOS = new DataOutputStream(clientBOS);
 
+        File video1 = getVideoFile("ANDK L1 01 IntroVideo");
+        File video2 = getVideoFile("L1 03 Dice Roller HSASC V4 V2");
+        File video3 = getVideoFile("L1 06 Creating The Dice Roller Project HS-SC");
+        File video4 = getVideoFile("L1 51 Adding The ImageView SC 1");
+        File[] videoCollection = {video1, video2, video3, video4};
+
+        VideosMovement videosMovement = new VideosMovement();
+        videosMovement.transferVideo(videoCollection, client);
+
         FileInputStream imageFileIS = new FileInputStream("C:\\Users\\Prosper's PC\\Pictures\\Screenshot_20200723_153336_com.prosperekwerike.pulsar.jpg");
         FileInputStream secondImageFileIS = new FileInputStream("C:\\Users\\Prosper's PC\\Pictures\\Screenshot_20200723_153307_com.prosperekwerike.pulsar.jpg");
         FileInputStream thirdImageFileIS = new FileInputStream("C:\\Users\\Prosper's PC\\Pictures\\Screenshot_20200723_153414_com.prosperekwerike.pulsar.jpg");
@@ -37,5 +46,9 @@ public class MainApplication {
 
     private static File createFile() {
         return new File("C:\\Users\\Prosper's PC\\Pictures\\serverc" + System.currentTimeMillis() + ".jpg");
+    }
+
+    private static File getVideoFile(String name){
+        return new File("C:\\Users\\Prosper's PC\\Desktop\\ANROID DEVELOPMENT COURSE\\LESSON 1" + name +".mp4");
     }
 }
--- src/VideosMovement.java
+++ src/VideosMovement.java
@@ -3,7 +3,7 @@ import java.net.Socket;
 
 public class VideosMovement {
 
-    void transferVideos(File[] videoCollection, Socket clientSocket) throws IOException {
+    void transferVideo(File[] videoCollection, Socket clientSocket) throws IOException {
         OutputStream clientSocketOS = clientSocket.getOutputStream();
         BufferedOutputStream clientSocketBOS = new BufferedOutputStream(clientSocketOS);
         DataOutputStream clientSocketDOS = new DataOutputStream(clientSocketBOS);
@@ -11,16 +11,24 @@ public class VideosMovement {
         //write the amount of video to transfer
         clientSocketDOS.writeInt(videoCollection.length);
 
-        // write the length of each video to the clientSocketDOS
+        // write the length and name of each video to the clientSocket DataOutputStream
         for(int i = 0; i < videoCollection.length; i++){
             clientSocketDOS.writeLong(videoCollection[i].length());
+            clientSocketDOS.flush();
+            clientSocketDOS.writeChars(videoCollection[i].getName());
         }
 
-        // write the bytes of each video to the clientSocketDOS
+        // write the bytes of each video to the clientSocket DataOutputStream
         for(int i = 0; i < videoCollection.length; i++){
             FileInputStream videoFileInputStream = new FileInputStream(videoCollection[i]);
             byte[] buffer = videoFileInputStream.readAllBytes();
             clientSocketDOS.write(buffer);
         }
+
+        clientSocketDOS.close();
+    }
+
+    void receiveVideos(){
+
     }
 }
