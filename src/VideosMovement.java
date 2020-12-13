552e4e27ec67c81196f76404e94eac5639373322
Prosper Ekwerike <pekwerike@gmail.com>
2020-12-13 19:52:52 +0100

    Building transferVideo function in VideosMovement class

--- src/VideosMovement.java
+++ src/VideosMovement.java
@@ -1,8 +1,26 @@
-import java.io.File;
+import java.io.*;
+import java.net.Socket;
 
 public class VideosMovement {
 
-    void transferVideos(File[] videoCollection){
+    void transferVideos(File[] videoCollection, Socket clientSocket) throws IOException {
+        OutputStream clientSocketOS = clientSocket.getOutputStream();
+        BufferedOutputStream clientSocketBOS = new BufferedOutputStream(clientSocketOS);
+        DataOutputStream clientSocketDOS = new DataOutputStream(clientSocketBOS);
 
+        //write the amount of video to transfer
+        clientSocketDOS.writeInt(videoCollection.length);
+
+        // write the length of each video to the clientSocketDOS
+        for(int i = 0; i < videoCollection.length; i++){
+            clientSocketDOS.writeLong(videoCollection[i].length());
+        }
+
+        // write the bytes of each video to the clientSocketDOS
+        for(int i = 0; i < videoCollection.length; i++){
+            FileInputStream videoFileInputStream = new FileInputStream(videoCollection[i]);
+            byte[] buffer = videoFileInputStream.readAllBytes();
+            clientSocketDOS.write(buffer);
+        }
     }
 }
