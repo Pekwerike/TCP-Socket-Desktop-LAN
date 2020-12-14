b07568fb2fca8999611cd05ccf2ace02730ec749
Prosper Ekwerike <pekwerike@gmail.com>
2020-12-14 11:47:34 +0100

    Created ClientMainApplication class and ClientVideosMovement class

---
+++ src/Client/ClientMainApplication.java
@@ -0,0 +1,22 @@
+package Client;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.Socket;
+
+public class ClientMainApplication {
+
+    public static void main(String[] args) throws IOException {
+        Socket server = new Socket("192.168.43.190", 8085);
+        System.out.println("Connected to server");
+
+        ClientVideosMovement videosMovement = new ClientVideosMovement();
+        videosMovement.receiveVideo(server);
+
+    }
+
+
+    private static File createFile() {
+        return new File("C:\\Users\\Prosper's PC\\Pictures\\new" + System.currentTimeMillis() + ".jpg");
+    }
+}
---
+++ src/Client/ClientVideosMovement.java
@@ -0,0 +1,40 @@
+package Client;
+
+import java.io.*;
+import java.net.Socket;
+
+public class ClientVideosMovement {
+
+    void receiveVideo(Socket server) throws IOException {
+        InputStream serverIS = server.getInputStream();
+        BufferedInputStream serverBIS = new BufferedInputStream(serverIS);
+        DataInputStream serverDIS = new DataInputStream(serverBIS);
+
+        // read the number of video file sent
+        int totalFiles = serverDIS.readInt();
+
+        int[] videosSize = new int[totalFiles];
+        String[] videosName = new String[totalFiles];
+
+        // read the names and length for each video
+        for (int i = 0; i < totalFiles; i++) {
+            videosSize[i] = (int) serverDIS.readLong();
+            videosName[i] = serverDIS.readUTF();
+        }
+
+        // read the bytes for each video
+        for (int i = 0; i < totalFiles; i++) {
+            byte[] buffer = new byte[videosSize[i]];
+            serverDIS.read(buffer, 0, videosSize[i]);
+            FileOutputStream receivedVideo = new FileOutputStream(createFile(videosName[i]));
+            receivedVideo.write(buffer);
+            receivedVideo.close();
+        }
+        System.out.println(String.format("%d videos received", totalFiles));
+
+    }
+
+    private static File createFile(String videoName) {
+        return new File("C:\\Users\\Prosper's PC\\Pictures\\" + videoName + ".mp4");
+    }
+}
