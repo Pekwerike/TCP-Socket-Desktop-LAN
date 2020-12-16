9e7f6f6c8db55a890f4e2390947994cd7c702d3c
Prosper Ekwerike <pekwerike@gmail.com>
2020-12-16 11:53:19 +0100

    Code refactor and created transferFile function in FileTransferProtocol class

---
+++ src/FileTransferProtocol/FileTransferProtocol.java
@@ -0,0 +1,15 @@
+package FileTransferProtocol;
+
+import java.net.Socket;
+
+public class FileTransferProtocol {
+    private Socket mSocket;
+
+    public FileTransferProtocol(Socket socket){
+        this.mSocket = socket;
+    }
+
+    public void transferFile(){
+
+    }
+}
