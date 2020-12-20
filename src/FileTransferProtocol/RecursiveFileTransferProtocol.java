2b416f4b5a30173d1aedd86807fb1630a4096921
Prosper Ekwerike <pekwerike@gmail.com>
2020-12-20 19:09:45 +0100

    Building transferFiles function in RecursiveFileTransferProtocol

--- src/FileTransferProtocol/RecursiveFileTransferProtocol.java
+++ src/FileTransferProtocol/RecursiveFileTransferProtocol.java
@@ -1,11 +1,34 @@
 package FileTransferProtocol;
 
+import java.io.*;
 import java.net.Socket;
 
 /**
  * This class makes use of the concept of recursion to transfer multiple files nested in several directories using
- * a single socket stream**/
+ * a single socket stream
+ **/
 public class RecursiveFileTransferProtocol {
+    private Socket mSocket;
 
-    public RecursiveFileTransferProtocol(Socket socket)
+    public RecursiveFileTransferProtocol(Socket socket) {
+        this.mSocket = socket;
+    }
+
+    public void transferFiles(File[] fileCollection) throws IOException {
+        OutputStream socketOS = mSocket.getOutputStream();
+        BufferedOutputStream socketBOS = new BufferedOutputStream(socketOS);
+        DataOutputStream socketDOS = new DataOutputStream(socketBOS);
+
+        int filesCount = 0;
+
+        for (int i = 0; i < fileCollection.length; i++) {
+            if (fileCollection[i].isDirectory()) {
+                filesCount += fileCollection[i].listFiles().length;
+            } else {
+                filesCount += 1;
+            }
+        }
+        // write the total number of files to transfer
+        socketDOS.writeInt(filesCount);
+    }
 }
