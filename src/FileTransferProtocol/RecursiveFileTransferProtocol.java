710de656d9c42e3347796c1840636061339ef2f1
Prosper Ekwerike <pekwerike@gmail.com>
2020-12-20 19:05:12 +0100

    Building RecursiveFileTransferProtocol class

---
+++ src/FileTransferProtocol/RecursiveFileTransferProtocol.java
@@ -0,0 +1,11 @@
+package FileTransferProtocol;
+
+import java.net.Socket;
+
+/**
+ * This class makes use of the concept of recursion to transfer multiple files nested in several directories using
+ * a single socket stream**/
+public class RecursiveFileTransferProtocol {
+
+    public RecursiveFileTransferProtocol(Socket socket)
+}
